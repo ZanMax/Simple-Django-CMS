350c3b135b447768958fc85365ffbc623fcb0aaf
maxim <zanoga.maxim@gmail.com>
2022-05-07 22:39:38 +0100

    update models, add Author

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, PostStatuses, PostTypes
+from .models import Post, PostStatuses, PostTypes, Author
 
 admin.site.site_header = "CMS Admin"
 admin.site.site_title = "CMS Admin"
@@ -9,10 +9,16 @@ admin.site.index_title = "Welcome to CMS"
 class PostAdmin(admin.ModelAdmin):
     fields = ('title', 'url', 'text', 'date',
               'post_modified', 'post_type', 'status')
-    prepopulated_fields = {'url': ('title',),}
+    prepopulated_fields = {"url": ("title",)}
     readonly_fields = ("post_modified",)
     list_filter = ("author", "post_type", "status",)
     list_display = ("title", "date", "author",)
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("articles_count", "experience",)
+    list_display = ("first_name", "last_name", "articles_count", "experience",)
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,50 @@
 from django.db import models
 import datetime
+from ckeditor.fields import RichTextField
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 # Create your models here.
 
 
 class PostStatuses(models.Model):
     status = models.CharField(max_length=32, unique=True, default='draft')
 
+    def __str__(self):
+        return self.status
+
 
 class PostTypes(models.Model):
     type = models.CharField(max_length=32, unique=True, default='post')
 
+    def __str__(self):
+        return self.type
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    articles_count = models.IntegerField(validators=[MinValueValidator(1)])
+    experience = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(50)])
+    age = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(90)])
+    education = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+    position = models.CharField(max_length=255)
+    expertise = models.CharField(max_length=255)
+    about = RichTextField()
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def __str__(self):
+        return self.full_name
 
 class Post(models.Model):
-    title = models.CharField(max_length=250)
+    title = models.CharField(max_length=255)
     date = models.DateTimeField()
     post_modified = models.DateTimeField()
     author = models.CharField(max_length=100)
-    text = models.TextField()
-    url = models.CharField(max_length=250, unique=True,
+    text = RichTextField()
+    url = models.SlugField(max_length=255, unique=True,
                            null=False, db_index=True)
     post_type = models.ForeignKey(
         PostTypes, default=1, on_delete=models.DO_NOTHING)
